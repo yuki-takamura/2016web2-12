340662d82712e950ab674426b4923719fdbdabbe
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-12-19 16:22:01 +0900

    Create generate_key.js

---
+++ routes/generate_key.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var app = require('../app.js');
+var crypto = require('crypto');
+
+router.get('/:encrypt , function(req, res, next) {
+var encrypt = req.params.encrypt;
+if(encrypt !== undefined)
+{
+var cipher = crypto.createCipher('aes192' , global.password);
+cipher.update(encrypt, 'utf8', 'hex');
+var cipheredText = cipher.final('hex');
+res.send(cipheredText);
+return;
+}
+});
+
+module.exports = router;
